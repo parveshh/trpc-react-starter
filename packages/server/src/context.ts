27d9be5110cfaa8ca07ea94339a198d32cccf132
Parvesh Malhotra <parveshh@gmail.com>
2023-12-28 13:35:17 +0000

    updated server configuration

--- packages/server/src/context.ts
+++ packages/server/src/context.ts
@@ -1,7 +1,10 @@
 import { User } from "@app/schemas";
 import { UserRepository } from "@app/dbclient";
+import { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
 
 export type TRPCContext = {
+  req: CreateFastifyContextOptions["req"];
+  res: CreateFastifyContextOptions["res"];
   user: User;
   userRepository: UserRepository;
 };
